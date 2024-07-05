f0c88d649e81fcd0e9b2ce38aaf4c64d4e7566fd
1u53r <143luser@gmail.com>
2024-07-05 08:11:38 +0530

    improvment

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let paper = document.querySelector(".paper");
 let rock = document.querySelector(".rock");
 let scisoor = document.querySelector(".scissor");
 let Message = document.querySelector(".message");
-
+let i = 0;
+let l = 0;
+let u = 0;
+let Won = document.querySelector("#won");
+let Lost = document.querySelector("#lost");
+let Draw = document.querySelector("#draw");
 function computer(random) {
     let computerSelect = "";
     if (random < 3) {
@@ -24,79 +29,85 @@ function main() {
 function me() {
     paper.addEventListener("click", () => {
         paper.style.border = "5px solid purple";
-        let won = document.querySelector(".won");
-        let lost = document.querySelector(".lost");
-        let draw = document.querySelector(".draw");
         document.querySelector(".scissor").style.border = "none";
         document.querySelector(".rock").style.border = "none";
-        let i = 0;
-        let l = 0;
-        let u = 0;
         let comp = main();
         if (comp == "scissor") {
-            console.log("YOu lost");
             l++;
+            console.log("You lost");
+            Lost.textContent = `Lost: ${l}`;
             Message.innerText = "Ahhh!! You lost Scissor Beats paper";
+            Message.style.backgoundColor = "red";
+
         } else if (comp == "rock") {
-            console.log("You won!!");
             i++;
+            console.log("You won!!");
+            Won.textContent = `Won: ${i}`;
             Message.innerText = "Hurray!! You won, Paper Beats Rock";
+            Message.style.backgoundColor = "green";
         } else if (comp == "paper") {
-            console.log("It's a draw");
             u++;
+            console.log("It's a draw");
+            Draw.textContent=`Draw: ${u}`;
             Message.innerText = "hun! It's a Draw, You both selected same";
+            Message.style.backgoundColor = "Yellow";
         }
 
     })
 
     rock.addEventListener("click", () => {
         rock.style.border = "5px solid purple";
-        let won = document.querySelector(".won");
-        let lost = document.querySelector(".lost");
-        let draw = document.querySelector(".draw");
         document.querySelector(".paper").style.border = "none";
         document.querySelector(".scissor").style.border = "none";
-        let i = 0;
-        let l = 0;
-        let u = 0;
         let comp = main();
         if (comp == "scissor") {
-            console.log("You won!!");
             i++;
+            Won.textContent = `Won: ${i}`;
+            console.log("You won!!");
             Message.innerText = "Hurray!! You won, Rock Beats Scissor";
+            Message.style.backgoundColor = "green";
         } else if (comp == "rock") {
-            console.log("It's a draw");
             u++;
+            console.log("It's a draw");
+            Draw.textContent=`Draw: ${u}`;
             Message.innerText = "hun! It's a Draw, You both selected same";
+            Message.style.backgoundColor = "Yellow";
+
         } else if (comp == "paper") {
-            console.log("You lost");
             l++;
+            Lost.textContent = `Lost: ${l}`;
+            console.log("You lost");
             Message.innerText = "Ahhh!! You lost Scissor Beats paper";
+            Message.style.backgoundColor = "red";
+
         }
     })
 
+
     scisoor.addEventListener("click", () => {
         scisoor.style.border = "5px solid purple";
-        let won = document.querySelector(".won");
-        let lost = document.querySelector(".lost");
-        let draw = document.querySelector(".draw");
         document.querySelector(".paper").style.border = "none";
         document.querySelector(".rock").style.border = "none";
-        let i = 0;
-        let l = 0;
-        let u = 0;
         let comp = main();
         if (comp == "scissor") {
-            console.log("It's a draw");
             u++;
-            Message.innerText = "hun! It's a Draw, You both selected same"
+            Draw.textContent=`Draw: ${u}`;
+            console.log("It's a draw",u);
+            Message.innerText = "hun! It's a Draw, You both selected same";
+            Message.style.backgoundColor = "Yellow";
+
         } else if (comp == "rock") {
-            console.log("You lost");
             l++;
+            console.log("You lost");
+            Lost.textContent = `Lost: ${l}`;
+            Message.innerText = "Ahhh!! You lost Scissor Beats Rock";
+            Message.style.backgoundColor = "red";
         } else if (comp == "paper") {
-            console.log("You won!!");
             i++;
+            Won.textContent = `Won: ${i}`;
+            console.log("You won!!");
             Message.innerText = "Hurray!! You won, Scissor Beats Paper";
+            Message.style.backgoundColor = "green";
         }
     })
 }
